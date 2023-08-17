5d2ad19dd8e6ca39144a19d006cc52c626e98771
aluhe garcia <aluhegarcia07@gmail.com>
2023-08-17 15:24:01 -0300

    se añadió angular

---
+++ curriculum/src/app/cursada/cursada.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-cursada',
+  templateUrl: './cursada.component.html',
+  styleUrls: ['./cursada.component.css']
+})
+export class CursadaComponent {
+
+}
